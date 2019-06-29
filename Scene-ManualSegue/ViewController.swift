96bbb8831933e27bf65bfca9a90cc02f556251bd
yunako <go2837@hanmail.net>
2019-06-29 17:56:03 +0900

    manualsegue test

---
+++ Scene-ManualSegue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scene-ManualSegue
+//
+//  Created by YUNA KO on 29/06/2019.
+//  Copyright © 2019 YUNAKO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    /*
+     @IBAction func wind(_ sender: Any) {
+        //세그웨이 실행
+       // self.performSegue(withIdentifier: "ManualWind", sender: self)
+    }*/
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue){
+    
+    }
+}
+
